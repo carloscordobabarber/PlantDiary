2f20b53a27e93563160cdd26efc4b4289a888824
carloscordobabarber <carlosernestocordobabarber@gmail.com>
2025-05-14 20:08:51 +0200

    85%

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,9 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation (libs.gson)
+    implementation (libs.glide)
+    annotationProcessor (libs.compiler)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
